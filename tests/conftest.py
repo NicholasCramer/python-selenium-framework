b98dfd0c98408b0fc5c39ed8eee15fad41fbe89d
Nicholas Cramer <73135495+NicholasCramer@users.noreply.github.com>
2024-03-08 15:47:21 -0700

    Update conftest
    Add --browser_name command line argument
    Updated setup fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,15 +2,27 @@ import pytest
 from selenium import webdriver
 
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser_name", action="store", default="chrome"
+    )
+
+
 @pytest.fixture(scope="class")
 def setup(request):
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option("detach", True)
-    browser = webdriver.Chrome(options=options)
+    browser_name = request.config.getoption("browser_name")
+    if browser_name == "chrome":
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option("detach", True)
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        browser = webdriver.Firefox()
+    elif browser_name == "edge":
+        browser = webdriver.Edge()
+
     browser.implicitly_wait(5)
     browser.maximize_window()
     browser.get("https://www.rahulshettyacademy.com/angularpractice/")
     request.cls.browser = browser
     yield
     browser.close()
-
