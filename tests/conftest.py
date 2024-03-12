39b282f620fb080035a27fd7669572ad9181a28e
Nicholas Cramer <73135495+NicholasCramer@users.noreply.github.com>
2024-03-12 17:00:40 -0600

    Update conftest.py
    Add functions to take a screenshot when tests fail and add the image to the html report

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium import webdriver
+browser = None
 
 
 def pytest_addoption(parser):
@@ -10,6 +11,7 @@ def pytest_addoption(parser):
 
 @pytest.fixture(scope="class")
 def setup(request):
+    global browser
     browser_name = request.config.getoption("browser_name")
     if browser_name == "chrome":
         options = webdriver.ChromeOptions()
@@ -26,3 +28,29 @@ def setup(request):
     request.cls.browser = browser
     yield
     browser.close()
+
+@pytest.mark.hookwrapper
+def pytest_runtest_makereport(item):
+    """
+    Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
+    :param item:
+    """
+
+    pytest_html = item.config.pluginmanager.getplugin('html')
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report, 'extra', [])
+
+    if report.when == 'call' or report.when == "setup":
+        xfail = hasattr(report, 'wasxfail')
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            file_name = report.nodeid.replace("::", "_")+".png"
+            _capture_screenshot(file_name)
+            if file_name:
+                html = '<div><img src="%s" alt="screenshot" style="width:340px;height:228px;" ' \
+                        'onclick="window.open(this.src)" align="right"/><div>' % file_name
+                extra.append(pytest_html.extras.html(html))
+            report.extra = extra
+
+def _capture_screenshot(name):
+    browser.get_screenshot_as_file(name)
