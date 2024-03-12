982020cb2a205ba6701bcab33f9765f6b3267852
Nicholas Cramer <73135495+NicholasCramer@users.noreply.github.com>
2024-03-12 16:07:15 -0600

    Remove hardcoded data from test
    Parameterized test_home_page and set up data file with multiple data sets

---
+++ TestData/home_page_data.py
@@ -0,0 +1,18 @@
+class HomePageData:
+
+    test_home_page_data = [
+        {
+            "first_name": "Nick",
+            "email": "nick_cramer@outlook.com",
+            "password": "password",
+            "gender": "Male",
+            "birthdate": "07141994"
+        },
+        {
+            "first_name": "Sarah",
+            "email": "Sarah@testmail.com",
+            "password": "password1",
+            "gender": "Female",
+            "birthdate": "08131990"
+        }
+    ]
--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -1,20 +1,28 @@
+import pytest
+from TestData.home_page_data import HomePageData
 from pages.home import HomePage
 from utilities.BaseClass import BaseClass
 
 
 class TestHomePage(BaseClass):
 
-    def test_form_submission(self):
+    def test_form_submission(self, get_data):
         home_page = HomePage(self.browser)
-        home_page.get_name().send_keys("Nick")
-        home_page.get_email().send_keys("nick_cramer@outlook.com")
-        home_page.get_password().send_keys("password")
+        home_page.get_name().send_keys(get_data["first_name"])
+        home_page.get_email().send_keys(get_data["email"])
+        home_page.get_password().send_keys(get_data["password"])
         home_page.get_checkbox().click()
-        self.select_option_by_text(home_page.get_gender(), "Male")
+        self.select_option_by_text(home_page.get_gender(), get_data["gender"])
         home_page.get_employment().click()
-        home_page.get_birth_date().send_keys("07141994")
+        home_page.get_birth_date().send_keys(get_data["birthdate"])
         home_page.submit_form().click()
 
         alert_text = home_page.get_success_message().text
 
         assert "Success" in alert_text
+
+        self.browser.refresh()
+
+    @pytest.fixture(params=HomePageData.test_home_page_data)
+    def get_data(self, request):
+        return request.param
